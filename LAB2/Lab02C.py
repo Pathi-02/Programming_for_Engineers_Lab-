ac8306947998dccf1564a1ef13edb6fef5b8ebd7
Pathi-02 <pathimilan03@gmail.com>
2024-08-15 12:46:14 +0530

    Update Lab02C.py

--- LAB2/Lab02C.py
+++ LAB2/Lab02C.py
@@ -1,52 +1,68 @@
-# Grading
-# CO1010 : Lab 02
-# Date : 15/08/2024
+# Get a numeric grade (0-100) from the user
+numeric_grade = int(input("Enter the numeric grade (0-100): "))
 
-# Read the marks  as float inputs
-marks= float(input("Enter the marks :"))
+# User selects the grading scheme
+grading_scheme = input("Enter 'SG' for Standard Grading or 'PMG' for Plus-Minus Grading: ").upper()
 
-gs = str(input("Enter the Grading schema method: ")).upper()
-#check the coditions
-if gs == "SG":
-    if 90< marks <=100:
-        grade = "A"
-    elif 80< marks <=89:
-        grade = "B"        
-    elif 70< marks <=79:
-        grade = "C"
-    elif 60< marks <=69:
-        grade = "D"
-    elif 0< marks <=59:
-        grade = "F"
-    print(f"user grade is{grade}")
-elif gs == "PMG":
-    if 97< marks <=100:
-        grade = "A+"
-    elif 93< marks <=96:
-        grade = "A"        
-    elif 90< marks <=92:
-        grade = "A-"
-    elif 87< marks <=89:
-        grade = "B+"
-    elif 83< marks <=86:
-        grade = "B"
-    elif 80< marks <=82:
-        grade = "B-"        
-    elif 77< marks <=79:
-        grade = "C+"
-    elif 73< marks <=76:
-        grade = "C"
-    elif 70< marks <=72:
-        grade = "C-"
-    elif 67< marks <=69:
-        grade = "D+"        
-    elif 63< marks <=66:
-        grade = "D"
-    elif 60< marks <=62:
-        grade = "D-"
-    elif 0< marks <=59:
-        grade = "F"
-    
-    
-    #Print the output
-    print(f"Your grade in {gs} Scheme is {grade}")
+# Determine the grade based on the selected scheme
+if grading_scheme == 'SG':
+    # Standard Grading Scheme
+    if 100 >= numeric_grade >= 90:
+        grade = 'A'
+    else:
+        if numeric_grade >= 80:
+            grade = 'B'
+        else:
+            if numeric_grade >= 70:
+                grade = 'C'
+            else:
+                if numeric_grade >= 60:
+                    grade = 'D'
+                else:
+                    grade = 'F'
+else:
+    # Plus-Minus Grading Scheme
+    if numeric_grade >= 97:
+        grade = 'A+'
+    else:
+        if numeric_grade >= 93:
+            grade = 'A'
+        else:
+            if numeric_grade >= 90:
+                grade = 'A-'
+            else:
+                if numeric_grade >= 87:
+                    grade = 'B+'
+                else:
+                    if numeric_grade >= 83:
+                        grade = 'B'
+                    else:
+                        if numeric_grade >= 80:
+                            grade = 'B-'
+                        else:
+                            if numeric_grade >= 77:
+                                grade = 'C+'
+                            else:
+                                if numeric_grade >= 73:
+                                    grade = 'C'
+                                else:
+                                    if numeric_grade >= 70:
+                                        grade = 'C-'
+                                    else:
+                                        if numeric_grade >= 67:
+                                            grade = 'D+'
+                                        else:
+                                            if numeric_grade >= 63:
+                                                grade = 'D'
+                                            else:
+                                                if numeric_grade >= 60:
+                                                    grade = 'D-'
+                                                else:
+                                                    grade = 'F'
+
+
+# Output the result
+if grade != "Invalid grading scheme":
+    print(f"The corresponding letter grade is {grade}.")
+else:
+    print(grade)
